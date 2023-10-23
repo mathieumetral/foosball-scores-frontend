2006f984df500ea2f9c95b30d413e9ea017e9140
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-23 19:11:53 +0200

    feat(navigation): add navbar component

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -7,8 +7,6 @@ export default function PlayersPage() {
     <div className="mb-16 mt-6">
       <h1 className="mb-4 text-2xl font-semibold">Players List</h1>
 
-      <hr className="mb-4 border-t-2 border-gray-200" />
-
       <div className="rounded-xl bg-white shadow">
         <PlayersList />
       </div>
