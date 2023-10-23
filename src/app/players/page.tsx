287978e946eff24d6c7852c7389c3bfabd91cd21
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-23 19:14:12 +0200

    feat(navigation): add navbar component (#8)
    * feat(navigation): add navbar component
    
    * chore(layout): add margin

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -4,14 +4,12 @@ export const dynamic = 'force-dynamic';
 
 export default function PlayersPage() {
   return (
-    <div className="mb-16 mt-6">
+    <>
       <h1 className="mb-4 text-2xl font-semibold">Players List</h1>
-
-      <hr className="mb-4 border-t-2 border-gray-200" />
 
       <div className="rounded-xl bg-white shadow">
         <PlayersList />
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/features/navigation/components/navbar.tsx
@@ -0,0 +1,22 @@
+interface Item {
+  path: string;
+  name: string;
+}
+
+const items: Item[] = [{path: '/players', name: 'Players'}];
+
+export function Navbar() {
+  return (
+    <nav className="bg-white p-4 shadow-md">
+      <ul className="flex justify-center space-x-6">
+        {items.map(item => (
+          <li key={item.path}>
+            <a href={item.path} className="hover:font-medium hover:text-blue-600">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
