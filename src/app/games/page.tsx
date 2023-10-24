e6cd45a622673903963207e12a4a4f5823563b18
Mathieu Métral <46543621+mathieumetral@users.noreply.github.com>
2023-10-24 17:11:12 +0200

    chore(pages): improve

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -9,9 +9,16 @@ export default function GamesPage() {
       <div className="flex items-center justify-between">
         <h1 className="mb-4 text-2xl font-semibold">Games List</h1>
 
-        <Link href="/games/create" prefetch={true} className="rounded-full bg-blue-600 px-4 py-1.5 text-sm text-white">
-          Create new game
-        </Link>
+        <div className="flex items-center">
+          <div className="mr-4 rounded-full bg-emerald-100 px-2 text-sm text-green-800">Use cache potential</div>
+          <Link
+            href="/games/create"
+            prefetch={true}
+            className="rounded-full bg-blue-600 px-4 py-1.5 text-sm text-white"
+          >
+            Create new game
+          </Link>
+        </div>
       </div>
 
       <GamesList />
--- src/features/navigation/components/navbar.tsx
+++ src/features/navigation/components/navbar.tsx
@@ -1,24 +1,40 @@
-interface Item {
-  path: string;
-  name: string;
-}
+'use client';
 
-const items: Item[] = [
-  {path: '/players', name: 'Players'},
-  {path: '/games', name: 'Games'},
-];
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
+import {clsx} from 'clsx';
 
 export function Navbar() {
+  const currentRoute = usePathname();
+
   return (
-    <nav className="bg-white p-4 shadow-md">
+    <nav className="bg-white shadow-md">
       <ul className="flex justify-center space-x-6">
-        {items.map(item => (
-          <li key={item.path}>
-            <a href={item.path} className="hover:font-medium hover:text-blue-600">
-              {item.name}
-            </a>
-          </li>
-        ))}
+        <li>
+          {/* Using a standard anchor tag to force a server-side refresh. We're doing this because the Relay cache isn't
+          always up-to-date after changes to matches for statistics. This isn't the most optimized approach, but it
+          simplifies things and allows us to benefit from caching on the CRUD games side only. */}
+          <a
+            href="/players"
+            className={clsx(
+              'block border-b-4 p-4 hover:font-medium',
+              currentRoute === '/players' ? 'border-blue-700' : 'border-transparent'
+            )}
+          >
+            Players
+          </a>
+        </li>
+        <li>
+          <Link
+            href="/games"
+            className={clsx(
+              'block border-b-4 p-4 hover:font-medium',
+              currentRoute.startsWith('/games') ? 'border-blue-700' : 'border-transparent'
+            )}
+          >
+            Games
+          </Link>
+        </li>
       </ul>
     </nav>
   );
